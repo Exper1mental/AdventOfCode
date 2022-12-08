39d9984d2cc30232b4de313ade5b17ece2a022c2
Thomas Delvaux <Exper1mental@users.noreply.github.com>
2022-12-08 06:59:56 -0500

    Added D7 and D8 (WIP)

---
+++ 2022/Day7_NoSpaceLeftOnDevice/d7_p2.py
@@ -0,0 +1,88 @@
+class MaxDirectorySize():
+    def __init__(self):
+        self.input_file = open('input.txt', 'r').readlines()
+        self.directory_dict = {}
+        self.current_path = []
+        self.last_call_dir = False
+        self.size_list = []
+        self.used_space = 0
+        
+    def main(self):
+        # Convert input to dictionary
+        for i, line in enumerate(self.input_file):
+            line = line.strip('\n')
+            str_list = line.split()
+            self.check_list(str_list)
+        
+        # Find directory sizes
+        self.get_dir_size(self.directory_dict, '')
+        max_space = 70000000
+        free_space = max_space-self.used_space
+        needed_space = 30000000
+        for size in sorted(self.size_list):
+            if free_space + size > needed_space:
+                print(size)
+                break
+
+    def get_dir_size(self, dir_dict, path):
+        keys = dir_dict.keys()
+        size = 0
+        for k in keys:
+            if dir_dict.get(k) is not None:
+                if dir_dict.get(k).get('filesize') is None:
+                    size += self.get_dir_size(dir_dict.get(k), path+' '+k)
+                else:
+                    size += int(dir_dict.get(k).get('filesize'))
+                    self.used_space += int(dir_dict.get(k).get('filesize'))
+        if dir_dict.get('filesize') is None:
+            self.size_list.append(size)
+        return size
+                    
+
+    def check_list(self, str_list):
+        if str_list[0] == '$':
+            self.check_list(str_list[1:])
+        
+        elif str_list[0] == 'cd':
+            if str_list[1] == '..':
+                self.current_path.pop()
+            elif str_list[1] == '/':
+                self.current_path = ['/']
+            else:
+                self.current_path.append(str_list[1])
+
+        elif str_list[0] in ['ls', 'dir']:
+            pass
+        else:
+            temp_dict = self.create_dict_entry(self.current_path, str_list)
+            self.directory_dict = self.merge_dict(self.directory_dict, temp_dict)
+        
+    def create_dict_entry(self, current_path, str_list):
+        temp_dict = {}
+        if len(current_path) > 1:
+            temp_dict = { current_path[0]: self.create_dict_entry(current_path[1:], str_list) }
+        else:
+            temp_dict = { current_path[0]: { str_list[1]: { 'filesize': int(str_list[0]) } } }
+        return temp_dict
+    
+    def merge_dict(self, dir_dict, temp_dict):
+        keys = dir_dict.keys() | temp_dict.keys()
+        loop_dict = {}
+        for k in keys:
+            if dir_dict.get(k) is not None and temp_dict.get(k) is not None:
+                if dir_dict.get(k).get('filesize') is None and temp_dict.get(k).get('filesize') is None:
+                    loop_dict = loop_dict | { k: self.merge_dict(dir_dict.get(k), temp_dict.get(k)) }
+                elif temp_dict.get(k).get('filesize') is not None:
+                    loop_dict = loop_dict | { k: temp_dict.get(k) }
+                elif dir_dict.get(k).get('filesize') is not None:
+                    loop_dict = loop_dict | { k: dir_dict.get(k) }
+                else:
+                    print('This should NOT happen!')
+            elif dir_dict.get(k) is None and temp_dict.get(k) is not None:
+                loop_dict = loop_dict | { k: temp_dict.get(k) }
+            elif dir_dict.get(k) is not None and temp_dict.get(k) is None:
+                loop_dict = loop_dict | { k: dir_dict.get(k) }
+        return loop_dict
+        
+if __name__ == '__main__':
+    MaxDirectorySize().main()
---
+++ 2022/Day8/d8_p1.py
@@ -0,0 +1,52 @@
+input_file = open('input.txt', 'r').readlines()
+rows = len(input_file)
+cols = len(input_file[0])
+tree_list = []
+visible_trees = 0
+hidden_trees = 0
+
+def is_shortest_tree(i, j, h):
+    left = False
+    right = False
+    top = False
+    bottom = False
+    
+    for k in range(len(input_file[i].strip('\n'))): # Check row
+        if k < j:
+            if h <= int(input_file[i][k]):
+                left = True
+        elif k > j:
+            if h <= int(input_file[i][k]):
+                right = True
+    for k in range(len(input_file)): # Check column
+        if k < i:
+            if h <= int(input_file[k][j]):
+                top = True
+        elif k > i:
+            if h <= int(input_file[k][j]):
+                bottom = True
+    
+    if left and right and top and bottom:
+        return True
+    
+    return False
+
+for i, line in enumerate(input_file):
+    line = line.strip('\n')
+    for j, tree_h in enumerate(line):
+        if i == 0 or i == rows-1 or j == 0 or j == cols-2:
+            visible_trees += 1
+        else:
+            if is_shortest_tree(i, j, int(tree_h)):
+                # print(i, j, tree_h)
+                hidden_trees += 1
+            else:
+                visible_trees += 1
+                
+
+print(visible_trees, hidden_trees)
+        
+        
+    
+# Tree visible if top, bottom, left, and right tree are shorter (same height means hidden)
+# All trees on edges are visible
